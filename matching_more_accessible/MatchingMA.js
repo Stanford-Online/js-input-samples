2e2b4b2d48d4322d1b2bc5e55102da7d6ad306a0
Chris <crodriguez@edx.org>
2014-11-21 13:13:15 -0500

    Adding aria to checkbox label

--- matching_more_accessible/MatchingMA.js
+++ matching_more_accessible/MatchingMA.js
@@ -294,7 +294,7 @@ function loadElementsSighted(){
     }
     
     $.each(elementsLeft, function(tableArrayLeft, i) {
-      options_options += '<li><input type="checkbox" id="element_' + tableArrayLeft + '_' + number + '" name="element' + tableArrayLeft + '" value="element' + number + '" /><label for="element_' + tableArrayLeft + '_' + number + '" class="option-label">' + this.label + '</label></li>';
+      options_options += '<li><input type="checkbox" id="element_' + tableArrayLeft + '_' + number + '" name="element' + tableArrayLeft + '" value="element' + number + '" /><label for="element_' + tableArrayLeft + '_' + number + '" class="option-label" aria-describedby="description' + tableArrayLeft + '">' + this.label + '</label></li>';
     });
 
     $(options_menu).append(options_options);
