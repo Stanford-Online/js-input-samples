2d7546acb3b92765f28161926a6d63d86eaa291c
Colin-Fredericks <colin.fredericks@gmail.com>
2017-09-20 14:59:09 -0400

    Initial commit for ordering questions

---
+++ ordering/Ordering_Elements.js
@@ -0,0 +1,83 @@
+// The label should be kept short for visual aesthetics.
+// Type can be 'text' or 'image'.
+// If the type is set to 'image', the 'text' field will be used as alt text for accessibility.
+// The imgURL should be just the filename; no need to include "/static/" in this.
+// The linked images should not be larger than about 700x700 pixels, or they will not display well.
+
+var elementsRight = {
+	'1': {
+		'label': 'First',
+		'type': 'text',
+		'text': 'Earliest Platform',
+		'imgURL': ''
+	},
+	'2': {
+		'label': 'Second',
+		'type': 'text',
+		'text': 'Second Platform',
+		'imgURL': ''
+	},
+	'3': {
+		'label': 'Third',
+		'type': 'text',
+		'text': 'Third Platform',
+		'imgURL': ''
+	},
+	'4': {
+		'label': 'Fourth',
+		'type': 'text',
+		'text': 'Fourth Platform',
+		'imgURL': ''
+	},
+	'5': {
+		'label': 'Fifth',
+		'type': 'text',
+		'text': 'Fifth Platform',
+		'imgURL': ''
+	},
+	'6': {
+		'label': 'Sixth',
+		'type': 'text',
+		'text': 'Most Recent Platform',
+		'imgURL': ''
+	}
+};
+
+var elementsLeft = {
+	'A': {
+		'label': 'NES',
+		'type': 'text',
+		'text': 'Nintendo Entertainment System',
+		'imgURL': ''
+	},
+	'B': {
+		'label': 'Wii',
+		'type': 'text',
+		'text': 'Nintendo Wii',
+		'imgURL': ''
+	},
+	'C': {
+		'label': 'SNES',
+		'type': 'image',
+		'text': 'Super Nintendo Entertainment System',
+		'imgURL': 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/SNES-Mod1-Console-Set.jpg/1200px-SNES-Mod1-Console-Set.jpg'
+	},
+	'D': {
+		'label': 'Master System',
+		'type': 'text',
+		'text': 'Nintendo Master System',
+		'imgURL': ''
+	},
+	'E': {
+		'label': 'Switch',
+		'type': 'text',
+		'text': 'Nintendo Switch',
+		'imgURL': ''
+	},
+	'F': {
+		'label': 'GameCube',
+		'type': 'image',
+		'text': 'Nintendo GameCube',
+		'imgURL': 'https://upload.wikimedia.org/wikipedia/commons/d/d1/GameCube-Set.jpg'
+	}
+};
