c3471a2a2cba7d76aa1a3ae3e4dffc40258cf618
Colin-Fredericks <colin.fredericks@gmail.com>
2019-10-25 12:10:09 -0400

    Work in progress

---
+++ sorting_hat/README.md
@@ -0,0 +1,37 @@
+## Sorting Hat Problem
+
+### Current Status
+
+WIP
+
+### Overview
+
+Builds off the Pathways problem to do a personality quiz.
+
+### Creating Your Own Sorting Hat Problem
+
+Here's how to adapt this example to create your own pathway problem.
+
+1. Download `sorting_hat.html` and `sorting_hat.js`, and put them in your Files & Uploads.
+2. Download `sorting_hat.xml`, follow the instructions at the top of that file, and paste the result into a Blank Advanced Problem. This will be much more involved than your typical problem - you need to do a lot here, including:
+   - Making the text
+   - Setting point values
+3. Make sure you've got `hx.js` and `hx.css` in your Files & Uploads. If you don't have those, grab them from the [HX-JS Repo](https://github.com/Colin-Fredericks/hx-js).
+4. You're done!
+
+### Customizing Your Sorting Hat Problem
+
+(add more here)
+
+### Using Multiple Sorting Hat Problems
+
+Probably not.
+
+### Files:
+
+Inside you will find many pieces used to create this problem:
+
+- `jschannel.js`: the javascript file that allows javascript-type problems to work at all. You should not need to change this. I didn't make this one.
+- `sorting_hat.js`: the javascript file that does the majority of the work. You should not need to change this.
+- `sorting_hat.html`: the HTML file that will be iframed into the page. You should not need to change this.
+- `sorting_hat.xml`: the XML used to create the problem within Studio. You will need to do a lot here.
---
+++ sorting_hat/sorting_hat.js
@@ -0,0 +1,125 @@
+var JSProblemState = {
+  currently_open: [],
+  last_open: [],
+  ever_opened: []
+};
+var disableStateUpdates = false;
+
+console.log('inner ready');
+
+// hx-js handles all the hide/show stuff for us.
+// Copying a few things from it below.
+
+// Takes in an array of classes, as from a className function.
+// Returns the number or name attached to the important class.
+function getClassNumber(className, importantClass) {
+  let allClasses = className.split(/\s+/);
+  for (let i = 0; i < allClasses.length; i++) {
+    if (allClasses[i].indexOf(importantClass) === 0) {
+      return allClasses[i].slice(importantClass.length);
+    }
+  }
+  return -1;
+}
+
+// Make sure all the boxes that were open last time are open now.
+function putEmBack() {
+  console.log('putting them back');
+  // Make sure hx-js has assigned listeners. We need those.
+  let waitForToggles = setInterval(function() {
+    console.log('checking for listeners');
+    if (window.parent.hxGlobalOptions) {
+      clearInterval(waitForToggles);
+      console.log('found them');
+      // Just click their buttons. Only click the first one we find.
+      JSProblemState.currently_open.forEach(function(n) {
+        if (
+          window.parent.window.$('.hx-toggletarget' + n).css('display') ===
+          'none'
+        ) {
+          console.log('element ' + n + ' is hidden. Clicking.');
+          disableStateUpdates = true;
+          window.parent.window.$('.hx-togglebutton' + n)[0].click();
+          disableStateUpdates = false;
+        }
+      });
+    }
+  }, 250);
+}
+
+// Keep the Problem State up to date.
+window.parent.window.$('[class^="hx-togglebutton"').on('click tap', function() {
+  if (disableStateUpdates) {
+    console.log('state updates temporarily disabled');
+  } else {
+    console.log('state updates aok');
+    JSProblemState.last_open = Array.from(JSProblemState.currently_open);
+
+    let myNumber = getClassNumber(this.className, 'hx-togglebutton');
+
+    // Every time we open a box, add it to ever_opened and currently_open.
+    if (JSProblemState.currently_open.indexOf(myNumber) === -1) {
+      JSProblemState.currently_open.push(myNumber);
+    } else {
+      JSProblemState.currently_open.splice(
+        JSProblemState.currently_open.indexOf(myNumber),
+        1
+      );
+    }
+
+    // Every time we close a box, remove it from currently_open.
+    if (JSProblemState.ever_opened.indexOf(myNumber) === -1) {
+      JSProblemState.ever_opened.push(myNumber);
+    }
+
+    console.log(JSProblemState);
+  }
+});
+
+// This wrapper function is necessary.
+var pathway_problem = (function() {
+  // REQUIRED --- DO NOT REMOVE/CHANGE!!
+  var channel;
+
+  // REQUIRED --- DO NOT REMOVE/CHANGE!!
+  if (window.parent !== window) {
+    channel = Channel.build({
+      window: window.parent,
+      origin: '*',
+      scope: 'JSInput'
+    });
+    channel.bind('getGrade', getGrade);
+    channel.bind('getState', getState);
+    channel.bind('setState', setState);
+  }
+
+  // getState() and setState() are required by the problem type.
+  function getState() {
+    console.log('getting state');
+    return JSON.stringify(JSProblemState);
+  }
+
+  function setState() {
+    console.log('setting state');
+    stateStr = arguments.length === 1 ? arguments[0] : arguments[1];
+    JSProblemState = JSON.parse(stateStr);
+    console.log(JSProblemState);
+    putEmBack();
+  }
+
+  function getGrade() {
+    console.log('getting grade');
+    // Log the problem state.
+    // This is called from the parent window's Javascript so that we can write to the official edX logs.
+    parent.logThatThing(JSProblemState);
+    // Return the whole problem state.
+    return JSON.stringify(JSProblemState);
+  }
+
+  // REQUIRED --- DO NOT REMOVE/CHANGE!!
+  return {
+    getState: getState,
+    setState: setState,
+    getGrade: getGrade
+  };
+})();
