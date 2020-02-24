2df6ce405b9207185730e809e64cb1b715e81f5a
Colin-Fredericks <colin.fredericks@gmail.com>
2020-02-24 16:44:27 -0500

    Damn near working

--- journaling/journaling.js
+++ journaling/journaling.js
@@ -3,12 +3,8 @@
 var JSProblemState = {
   answer: '',
   saveslot: '',
-  length: ''
+  length: 0
 };
-
-// Pull in functions from the editor code
-var hxSetData = parent.hxSetData;
-var hxGetData = parent.hxGetData;
 
 // Save slot is passed as a URL parameter.
 function getUrlVars() {
@@ -44,20 +40,33 @@ var journaling = (function() {
     channel.bind('setState', setState);
   }
 
+  function getStateData() {
+    // Use what the learner typed, not what's in the save buffer.
+    // Include markup and everything.
+    let markup_string = parent.HXED.getMarkupFrom(JSProblemState.saveslot);
+    // Also set length equal to number of characters typed.
+    // Ignore markup for this.
+    let len = parent.$(markup_string).text().length;
+    let save_slot = JSProblemState.saveslot;
+    return {
+      answer: markup_string,
+      saveslot: save_slot,
+      length: len
+    };
+  }
+
   // Called by edX to obtain the current learner state for this problem.
   function getState() {
     console.log('getting state');
-    // Get what the learner typed.
-    let markup_string = parent.window
-      .$('[data-saveslot="' + JSProblemState.saveslot + '"] .summernote')
-      .summernote('code');
-    console.log(markup_string);
-    let len = parent.$(markup_string).text().length;
+    // Get the current state data.
+    JSProblemState = getStateData();
+    console.log(JSProblemState);
     // Save the current buffer.
-    hxSetData('summernote_' + JSProblemState.saveslot, markup_string);
+    parent.hxSetData(
+      'summernote_' + JSProblemState.saveslot,
+      JSProblemState.answer
+    );
     // Give edX our state as a string.
-    JSProblemState.answer = markup_string;
-    JSProblemState.length = len;
     console.log(JSProblemState);
     return JSON.stringify(JSProblemState);
   }
@@ -65,53 +74,22 @@ var journaling = (function() {
   // Called by edX to set the live learner state to what's recorded on the server.
   function setState() {
     console.log('setting state');
-    // Reactivate the summernote editor. Problem reset wipes it out.
-    parent.HXED.activateEditor(JSProblemState.saveslot);
-    console.log('breakpoint 1');
     // Make sure we're getting the right thing from edX.
     stateStr = arguments.length === 1 ? arguments[0] : arguments[1];
-    console.log('breakpoint 2');
     // edX stores the state as stringified JSON. Parse it.
-    console.log(stateStr);
     JSProblemState = JSON.parse(stateStr);
-    console.log('breakpoint 3');
-    // What if the stored state doesn't match the backpack's data? Give choice.
-    if (
-      !parent._.isEqual(
-        hxGetData('summernote_' + JSProblemState.saveslot),
-        JSProblemState.answer
-      )
-    ) {
-      console.log('breakpoint 4');
-      if (
-        confirm(
-          `It looks like you updated your journal somewhere else.
-          Press OK to use it, or Cancel to use your old answer to this problem.`
-        )
-      ) {
-        JSProblemState.answer = hxGetData(
-          'summernote_' + JSProblemState.saveslot
-        );
-      } else {
-        hxSetData(
-          'summernote_' + JSProblemState.saveslot,
-          JSProblemState.answer
-        );
-        let markup_string = parent.window
-          .$('[data-saveslot="' + JSProblemState.saveslot + '"] .summernote')
-          .summernote('code', JSProblemState.answer);
-      }
-    }
+    console.log(JSProblemState);
+    // The editor will automatically pull the last stored text.
+    // No need to repeat that functionality here.
+    // (What if the stored state doesn't match the backpack's data?)
   }
 
   // Called by edX when the learner submits the problem.
   function getGrade() {
     console.log('getting grade');
-    // Get what the learner typed.
-    let markup_string = parent
-      .$('[data-saveslot="' + JSProblemState.saveslot + '"] .summernote')
-      .summernote('code');
-    JSProblemState.answer = markup_string;
+    // Get the current state data.
+    JSProblemState = getStateData();
+    console.log(JSProblemState);
     // This calls the logging code from the XML file.
     parent.logThatThing(JSProblemState);
     // Send the problem state to be graded.
@@ -128,4 +106,4 @@ var journaling = (function() {
 })();
 
 // Just letting us know that the iframe is working.
-console.log('inner ready: journaling assignment');
+console.log('inner ready');
