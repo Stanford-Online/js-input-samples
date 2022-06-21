2ee53c1bff1460da3626d5e088a1b480007e0333
Colin-Fredericks <Colin-Fredericks@users.noreply.github.com>
2022-06-21 10:59:45 -0400

    Removing user IDs and autoformatting

--- qualtrics_grader/qualtrics_grader.js
+++ qualtrics_grader/qualtrics_grader.js
@@ -1,40 +1,46 @@
 var JSProblemState = {
   score: 0,
-  survey_url: 'unknown',
-  survey_complete: false
+  survey_url: "unknown",
+  survey_complete: false,
 };
 
 var postSurveyHTML =
   '<p style="font-family: Open Sans,Helvetica,Arial,sans-serif;">' +
-  'You have already taken this survey.';
+  "You have already taken this survey.";
 
 if (parent.allowReset) {
   postSurveyHTML +=
     ' If you would like to change your answers, use the "Reset" button at the bottom to restart the survey from the beginning.';
 }
 
-postSurveyHTML += '</p>';
+postSurveyHTML += "</p>";
 
-$(document).ready(function() {
-  console.log('inner ready');
+$(document).ready(function () {
+  console.log("inner ready");
 
   // Give the iframe an appropriate title attribute.
-  parent.$('iframe[name=' + window.name + ']').attr('title', 'Embedded Survey');
+  parent.$("iframe[name=" + window.name + "]").attr("title", "Embedded Survey");
 
-  // Making sure the analytics functions are ready. Usually very quick.
-  var waitForAnalytics = setInterval(function() {
-    if (parent.analytics._user._getId() && parent.qualtricsSurveySource) {
-      clearInterval(waitForAnalytics);
-
-      var anonID = parent.analytics._user._getId();
-      var qualtricsSurveySource =
-        parent.qualtricsSurveySource + '?uid=' + anonID;
+  // Making sure the survey source is set. Usually very quick.
+  let timer = 0;
+  var waitForSource = setInterval(function () {
+    if (parent.qualtricsSurveySource) {
+      clearInterval(waitForSource);
 
       // Set the survey URL from the parent page, including student ID.
       // We're doing this to make it easier to set all the options in the XML,
       // instead of having to have a separate .html page for every question.
-      $('#QualtricsSurvey').attr('src', qualtricsSurveySource);
-      JSProblemState.survey_url = qualtricsSurveySource;
+      $("#QualtricsSurvey").attr("src", parent.qualtricsSurveySource);
+      JSProblemState.survey_url = parent.qualtricsSurveySource;
+    }
+    // Give it 3 seconds to find the survey location variable; log error if you can't.
+    timer += 0.25;
+    if (timer > 3) {
+      clearInterval(waitForSource);
+      console.log(
+        "Couldn't find qualtricsSurveySource variable - check your problem setup."
+      );
+      $("body").html("<p>This problem has a configuration issue.</p>");
     }
   }, 250);
 
@@ -42,15 +48,15 @@ $(document).ready(function() {
   // Verify source and store info.
   function hearQualtricsSurveyEnd(e) {
     // Only accept from Qualtrics.
-    if (e.origin !== 'https://harvard.az1.qualtrics.com') {
+    if (e.origin !== "https://harvard.az1.qualtrics.com") {
       return;
     }
 
     // Only accept objects with the right form.
-    if (typeof e.data == 'string') {
+    if (typeof e.data == "string") {
       return;
     } else {
-      if (e.data.source === 'HX_Qualtrics_Survey') {
+      if (e.data.source === "HX_Qualtrics_Survey") {
         JSProblemState.score = Math.max(
           JSProblemState.score,
           e.data.score / 100
@@ -59,11 +65,11 @@ $(document).ready(function() {
     }
   }
 
-  addEventListener('message', hearQualtricsSurveyEnd, false);
+  addEventListener("message", hearQualtricsSurveyEnd, false);
 });
 
 // This wrapper function is necessary.
-var qualtrics_grader = (function() {
+var qualtrics_grader = (function () {
   // REQUIRED --- DO NOT REMOVE/CHANGE!!
   var channel;
 
@@ -71,35 +77,35 @@ var qualtrics_grader = (function() {
   if (window.parent !== window) {
     channel = Channel.build({
       window: window.parent,
-      origin: '*',
-      scope: 'JSInput'
+      origin: "*",
+      scope: "JSInput",
     });
-    channel.bind('getGrade', getGrade);
-    channel.bind('getState', getState);
-    channel.bind('setState', setState);
+    channel.bind("getGrade", getGrade);
+    channel.bind("getState", getState);
+    channel.bind("setState", setState);
   }
 
   // getState() and setState() are required by the problem type.
   function getState() {
-    console.log('getting state');
+    console.log("getting state");
     return JSON.stringify(JSProblemState);
   }
 
   function setState() {
-    console.log('setting state');
+    console.log("setting state");
     stateStr = arguments.length === 1 ? arguments[0] : arguments[1];
     JSProblemState = JSON.parse(stateStr);
     if (JSProblemState.survey_complete) {
-      console.log('Already took survey.');
-      $('#QualtricsSurvey').remove();
-      $('body').html(postSurveyHTML);
-      $('body').css('background-color', '#ccc');
+      console.log("Already took survey.");
+      $("#QualtricsSurvey").remove();
+      $("body").html(postSurveyHTML);
+      $("body").css("background-color", "#ccc");
       return;
     }
   }
 
   function getGrade() {
-    console.log('getting grade');
+    console.log("getting grade");
     JSProblemState.survey_complete = true;
     // Log the problem state.
     // This is called from the parent window's Javascript so that we can write to the official edX logs.
@@ -112,6 +118,6 @@ var qualtrics_grader = (function() {
   return {
     getState: getState,
     setState: setState,
-    getGrade: getGrade
+    getGrade: getGrade,
   };
 })();
